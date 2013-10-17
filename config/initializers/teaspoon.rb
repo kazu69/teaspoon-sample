0be740f34b021a217112b5f83062773cb7fc54da
kazu69 <kazu69web@gmail.com>
2013-10-17 23:31:39 +0900

    remove setting

--- config/initializers/teaspoon.rb
+++ config/initializers/teaspoon.rb
@@ -53,8 +53,6 @@ Teaspoon.setup do |config|
     # Accepts an array of filenames or regular expressions. The default is to exclude assets from vendors or gems.
     suite.no_coverage = [%r{/lib/ruby/gems/}, %r{/vendor/assets/}, %r{/support/}, %r{/(.+)_helper.}]
     # suite.no_coverage << "jquery.min.js" # excludes jquery from coverage reports
-
-    #config.driver = "selenium"
   end
 
   # Example suite. Since we're just filtering to files already within the root spec/javascripts, these files will also
